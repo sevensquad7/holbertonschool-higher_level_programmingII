e33664d3753322bf849960dc709916847999028a
zye7ert <edgarxot@gmail.com>
2021-09-19 19:33:38 -0500

    update the code of the print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,18 +3,18 @@
 if __name__ == "__main__":
     import sys
 
-    l = len(sys.argv) - 1
+    lenarg = len(sys.argv) - 1
     text = "arguments"
     sig = ":"
     argv = sys.argv
 
-    if l == 0:
+    if lenarg == 0:
         sig = "."
 
-    if l == 1:
+    if lenarg == 1:
         text = "argument"
 
-    print("{:d} {}{}".format(l, text, sig))
+    print("{:d} {}{}".format(lenarg, text, sig))
 
-    for i in range(1, l + 1):
+    for i in range(1, lenarg + 1):
         print("{:d}: {}".format(i, argv[i]))
